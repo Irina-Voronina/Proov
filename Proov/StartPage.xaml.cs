7c2852b853e1696205a3f033bc549124f3e733bb
Irina Voronina <irinaworonina@gmail.com>
2023-02-10 10:06:48 +0200

    Add project files.

---
+++ Proov/StartPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Proov
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StartPage : ContentPage
+    {
+        public StartPage()
+        {
+            //InitializeComponent();
+            Button Entry_btn = new Button
+            {
+                Text = "Ava Entry leht",
+                TextColor = Color.Tomato,
+                BackgroundColor = Color.Violet
+            };
+
+            Button Timer_btn = new Button
+            {
+                Text = "Ava Timer leht",
+                TextColor = Color.Tomato,
+                BackgroundColor = Color.Violet
+            };
+
+            StackLayout st = new StackLayout
+            {
+                Orientation = StackOrientation.Vertical,
+                Children = { Entry_btn, Timer_btn },
+                BackgroundColor = Color.Yellow
+            };
+            Content = st;
+            Entry_btn.Clicked += Entry_btn_Clicked;
+            Timer_btn.Clicked += Timer_btn_Clicked;
+        }
+
+        private async void Timer_btn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Timer_Page());
+            //throw new NotImplementedException();
+        }
+
+        private async void Entry_btn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Entry_Page());
+        }
+    }
+}
