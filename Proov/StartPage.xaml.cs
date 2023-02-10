c3d89c021fbae9f42970c47495427c3453d947f6
Irina Voronina <irinaworonina@gmail.com>
2023-02-10 11:18:22 +0200

    10/02/2023

--- Proov/StartPage.xaml.cs
+++ Proov/StartPage.xaml.cs
@@ -29,21 +29,34 @@ namespace Proov
                 BackgroundColor = Color.Violet
             };
 
+            Button Box_btn = new Button
+            {
+                Text = "Ava BoxView leht",
+                TextColor = Color.Tomato,
+                BackgroundColor = Color.Violet
+            };
+
             StackLayout st = new StackLayout
             {
                 Orientation = StackOrientation.Vertical,
-                Children = { Entry_btn, Timer_btn },
+                Children = { Entry_btn, Timer_btn, Box_btn },
                 BackgroundColor = Color.Yellow
             };
             Content = st;
             Entry_btn.Clicked += Entry_btn_Clicked;
             Timer_btn.Clicked += Timer_btn_Clicked;
+            Box_btn.Clicked += Box_btn_Clicked;
+        }
+
+        private async void Box_btn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new BoxView_Page());
+            //throw new NotImplementedException();
         }
 
         private async void Timer_btn_Clicked(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new Timer_Page());
-            //throw new NotImplementedException();
         }
 
         private async void Entry_btn_Clicked(object sender, EventArgs e)
